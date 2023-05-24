e5ec874046d83e0669650cf1f25169fede89d1dc
coutram <coutram@users.noreply.github.com>
2023-05-23 20:56:54 -0500

    all remove backend/models

--- sports-app/src/Orders.js
+++ sports-app/src/Orders.js
@@ -18,17 +18,36 @@ function preventDefault(event) {
   event.preventDefault();
 }
 
-async function getData(props) {
-  const resp = await fetch(
-    'http://localhost:5000/', {
-        method: "get",
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })
+function descendingComparator(a, b, orderBy) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
 
-    const result = await resp.json()
-    return result
+// Since 2020 all major browsers ensure sort stability with Array.prototype.sort().
+// stableSort() brings sort stability to non-modern browsers (notably IE11). If you
+// only support modern browsers you can replace stableSort(exampleArray, exampleComparator)
+// with exampleArray.slice().sort(exampleComparator)
+function stableSort(array, comparator) {
+  const stabilizedThis = array.map((el, index) => [el, index]);
+  stabilizedThis.sort((a, b) => {
+    const order = comparator(a[0], b[0]);
+    if (order !== 0) {
+      return order;
+    }
+    return a[1] - b[1];
+  });
+  return stabilizedThis.map((el) => el[0]);
+}
+
+function getComparator(order, orderBy) {
+  return order === 'desc'
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
 }
 
 function ratingAlgo (row)  { 
@@ -40,6 +59,10 @@ function Orders() {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+
+  const createSortHandler = (property) => (event) => {
+    onRequestSort(event, property);
+  };
 
   useEffect(() => {
     const getData = async () => {
@@ -70,14 +93,14 @@ function Orders() {
 
    return (
     <React.Fragment>
-        <Title>Recent Orders</Title>
+        <Title>Sports Team ArvOutram Rating</Title>
         <Table size="small">
           <TableHead>
             <TableRow>
-              <TableCell>Sports Team</TableCell>
-              <TableCell>Wins</TableCell>
-              <TableCell>Win %</TableCell>
-              <TableCell align="right">Ratings</TableCell>
+              <TableCell><b>Sports Team</b></TableCell>
+              <TableCell><b>Wins</b></TableCell>
+              <TableCell><b>Win %</b></TableCell>
+              <TableCell align="right"><b>Ratings</b></TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
